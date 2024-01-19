c9cf3ff5cd916e48ac0e2c75c49cc081039533dc
Kuldeep-045 <86959046+Kuldeep-045@users.noreply.github.com>
2024-01-19 16:07:42 +0530

    homePage responsive

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,51 +1,90 @@
-import React from 'react'
-import {  CiSearch } from "react-icons/ci";
+import React from "react";
+import { CiSearch } from "react-icons/ci";
 import { FaLocationDot } from "react-icons/fa6";
 import { SlCalender } from "react-icons/sl";
 import { RiMotorbikeFill } from "react-icons/ri";
 // import { RiMotorbikeFill } from "react-icons/ri";
 
 const Booking = () => {
-  return (
-    <div id='booking'>
-      <div>
-        <h1>Book A Bike</h1>
-        <div class="bookingContainer">
-    <section>
-        <h6><RiMotorbikeFill /> Enter Your Bike Type <span>*</span></h6>
-        <input type="text" id="myText" name="myText" placeholder="Enter your bike type" />
-    </section>
+    return (
+        <div id="booking">
+            <div>
+                <h1>Book A Bike</h1>
+                <div class="bookingContainer">
+                    <section>
+                        <h6>
+                            <RiMotorbikeFill /> Enter Your Bike Type{" "}
+                            <span>*</span>
+                        </h6>
+                        <input
+                            type="text"
+                            id="myText"
+                            name="myText"
+                            placeholder="Enter your bike type"
+                        />
+                    </section>
 
-    <section>
-        <h6><FaLocationDot /> Pick-up <span>*</span></h6>
-        <input type="text" id="myText" name="myText" placeholder="Enter pick up location" />
-    </section>
+                    <section>
+                        <h6>
+                            <FaLocationDot /> Pick-up <span>*</span>
+                        </h6>
+                        <input
+                            type="text"
+                            id="myText"
+                            name="myText"
+                            placeholder="Enter pick up location"
+                        />
+                    </section>
 
-    <section>
-        <h6><FaLocationDot />   Drop-of <span>*</span></h6>
-        <input type="text" id="myText" name="myText" placeholder="Enter drop off Location" />
-    </section>
-</div>
+                    <section>
+                        <h6>
+                            <FaLocationDot /> Drop-of <span>*</span>
+                        </h6>
+                        <input
+                            type="text"
+                            id="myText"
+                            name="myText"
+                            placeholder="Enter drop off Location"
+                        />
+                    </section>
+                {/* </div> */}
 
-<div class="bookingContainer">
-    <section>
-        <h6> <SlCalender /> Pick-up <span>*</span></h6>
-        <input type="text" id="myText" name="myText" placeholder="dd-mm-yyyy" />
-    </section>
+                {/* <div class="bookingContainer"> */}
+                    <section>
+                        <h6>
+                            {" "}
+                            <SlCalender /> Pick-up <span>*</span>
+                        </h6>
+                        <input
+                            type="text"
+                            id="myText"
+                            name="myText"
+                            placeholder="dd-mm-yyyy"
+                        />
+                    </section>
 
-    <section>
-        <h6><SlCalender /> Drop-of <span>*</span></h6>
-        <input type="text" id="myText" name="myText" placeholder="dd-mm-yyyy" />
-    </section>
+                    <section>
+                        <h6>
+                            <SlCalender /> Drop-of <span>*</span>
+                        </h6>
+                        <input
+                            type="text"
+                            id="myText"
+                            name="myText"
+                            placeholder="dd-mm-yyyy"
+                        />
+                    </section>
 
-    <section>
-        <h6><CiSearch />  </h6>
-        <button>Search</button>
-    </section>
-</div>
-      </div>
-    </div>
-  )
-}
+                    <section>
+                        <h6>
+                            <CiSearch />{" "}
+                        </h6>
+                        <button>Search</button>
+                    </section>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Booking
+export default Booking;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,54 @@
 import React from "react";
-import homeImage from "../assets/home.png"
+import homeImage from "../assets/home.png";
 // import Hunter from "../assets/Hunter.jpg"
 import { CiCircleCheck } from "react-icons/ci";
 import { FaAngleRight } from "react-icons/fa6";
 
-import Booking from './Booking';
-import Planning from './Planning';
-import Model from './Model'
-import Screen from './Screen'
-import Choose from './Choose'
-import Reviews from './Reviews.jsx'
-import FAQ from './FAQ.jsx'
-import Footer from './Footer.jsx'
+import Booking from "./Booking";
+import Planning from "./Planning";
+import Model from "./Model";
+import Screen from "./Screen";
+import Choose from "./Choose";
+import Reviews from "./Reviews.jsx";
+import FAQ from "./FAQ.jsx";
+import Footer from "./Footer.jsx";
 
 const Home = () => {
     return (
         <>
-        <div id="home">
-            <div>
-                <h2>Plan your trip now</h2>
-                <h1>Save <span>big</span> with our bike rental</h1>
-                <p>
-                    Rent the bike of your dreams. Unbeatable prices, unlimited
-                    miles, flexible pick-up options and much more.
-                </p>
-                <button>Book Ride <CiCircleCheck /></button>
-                <button className="homeBtn1">Learn More <FaAngleRight/></button>
+            <div id="home">
+                <div>
+                    <h2>Plan your trip now</h2>
+                    <h1>
+                        Save <span>big</span> with our bike rental
+                    </h1>
+                    <p>
+                        Rent the bike of your dreams. Unbeatable prices,
+                        unlimited miles, flexible pick-up options and much more.
+                    </p>
+                    <div className="buttonContainer">
+                        <button>
+                            Book Ride <CiCircleCheck />
+                        </button>
+                        <button className="homeBtn1">
+                            Learn More <FaAngleRight />
+                        </button>
+                    </div>
+                    {/* <button>Book Ride <CiCircleCheck /></button> */}
+                    {/* <button className="homeBtn1">Learn More <FaAngleRight/></button> */}
+                </div>
+                <div className="bikeImageHome">
+                    <img src={homeImage} alt="bike" />
+                </div>
             </div>
-            <div className="bikeImageHome">
-                <img src={homeImage} alt="bike" />
-            </div>
-        </div>
-      <Booking/>
-      <Planning/>
-      <Model/>
-      <Screen/>
-      <Choose/>
-      <Reviews/>
-      <FAQ/>
-      {/* <Footer/>  */}
-
+            <Booking />
+            <Planning />
+            {/* <Model/> */}
+            <Screen />
+            <Choose />
+            <Reviews />
+            <FAQ />
+            {/* <Footer/>  */}
         </>
     );
 };
